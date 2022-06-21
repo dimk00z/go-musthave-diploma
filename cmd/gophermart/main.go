025a41693edf49346a6458e8f19538e41e3a9d09
Dimk00z <dimk00z@gmail.com>
2022-06-21 15:57:39 +0300

    Test logger

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/dimk00z/go-musthave-diploma/config"
 	"github.com/dimk00z/go-musthave-diploma/internal/app"
+	"github.com/rs/zerolog"
 )
 
 func main() {
 	// Configuration
+	logger := zerolog.New(os.Stdout).Level(zerolog.DebugLevel)
+
+	logger.Log().Msg("starting App")
+
 	cfg, err := config.NewConfig()
 	if err != nil {
 		log.Fatalf("Config error: %s", err)
